18da50af569841c9840d3ed755a78781aa7292e4
Brian Hicks <brian@brianthicks.com>
2020-11-06 14:00:10 -0600

    load the tree sitter grammar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use anyhow;
 use std::process;
+use thiserror::Error;
+use tree_sitter;
 
 fn main() {
     if let Err(err) = real_main() {
@@ -8,5 +11,32 @@ fn main() {
 }
 
 fn real_main() -> anyhow::Result<()> {
+    let _parser = elm_parser();
     Ok(())
+}
+
+// tree-sitter setup
+
+extern "C" {
+    fn tree_sitter_elm() -> tree_sitter::Language;
+}
+
+fn language_elm() -> tree_sitter::Language {
+    unsafe { tree_sitter_elm() }
+}
+
+fn elm_parser() -> anyhow::Result<tree_sitter::Parser> {
+    let mut parser = tree_sitter::Parser::new();
+
+    parser
+        .set_language(language_elm())
+        .map_err(LanguageError::LanguageError)?;
+
+    Ok(parser)
+}
+
+#[derive(Error, Debug)]
+enum LanguageError {
+    #[error("tree sitter language error: {0:?}")]
+    LanguageError(tree_sitter::LanguageError),
 }
