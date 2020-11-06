1dae29b1fc587b43f4b11780b4897563db7f9bf1
Brian Hicks <brian@brianthicks.com>
2020-11-06 13:31:40 -0600

    add tree-sitter dependencies

---
+++ build.rs
@@ -0,0 +1,19 @@
+use std::path::PathBuf;
+
+// https://doc.rust-lang.org/cargo/reference/build-scripts.html
+fn main() {
+    let dir: PathBuf = ["vendor", "tree-sitter-elm", "src"].iter().collect();
+
+    println!("cargo:rerun-if-changed=vendor/tree-sitter-elm/src/parser.c");
+    cc::Build::new()
+        .include(&dir)
+        .file(dir.join("parser.c"))
+        .compile("tree-sitter-elm");
+
+    println!("cargo:rerun-if-changed=vendor/tree-sitter-elm/src/scanner.cc");
+    cc::Build::new()
+        .include(&dir)
+        .cpp(true)
+        .file(dir.join("scanner.cc"))
+        .compile("tree_sitter_elm_scanner")
+}
