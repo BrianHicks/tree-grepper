a30fa194e832caf6d01e20960386206488ce15e5
Brian Hicks <brian@brianthicks.com>
2021-01-07 09:06:16 -0600

    make tree-grepper-dump work with multiple languages too

--- src/bin/tree-grepper-dump.rs
+++ src/bin/tree-grepper-dump.rs
@@ -1,7 +1,10 @@
 use clap::Clap;
+use std::fmt;
+use std::fmt::Display;
 use std::fs;
 use std::path::PathBuf;
 use std::process;
+use std::str::FromStr;
 
 #[derive(Clap, Debug)]
 /// Dump a file to an s-expression so you can see what things you can
@@ -9,12 +12,18 @@ use std::process;
 struct Opts {
     /// Which file should we dump?
     file: PathBuf,
+
+    /// What language should we require? (This is a required flag, but may be
+    /// optional later if we add something that can determine file type or just
+    /// try them all in sequence.)
+    #[clap(short('l'), long)]
+    language: Language,
 }
 
 fn main() {
     let opts = Opts::parse();
 
-    let mut parser = match parser(language_elm()) {
+    let mut parser = match opts.language.parser() {
         Ok(p) => p,
         Err(err) => {
             eprintln!(
@@ -45,21 +54,65 @@ fn main() {
 }
 
 // tree-sitter setup
+//
+// TODO: this is all duplicated from tree-grepper.rs. Once I figure out how
+// the module system works with separate binaries I should go back and pull
+// this into a shared crate.
+
+#[derive(Debug)]
+enum Language {
+    Elm,
+    Ruby,
+}
+
+impl FromStr for Language {
+    type Err = LanguageError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "elm" => Ok(Language::Elm),
+            "ruby" => Ok(Language::Ruby),
+            _ => Err(LanguageError::UnknownLanguage),
+        }
+    }
+}
+
+impl Language {
+    fn parser(&self) -> Result<tree_sitter::Parser, tree_sitter::LanguageError> {
+        let mut parser = tree_sitter::Parser::new();
+
+        parser.set_language(match self {
+            Language::Elm => language_elm(),
+            Language::Ruby => language_ruby(),
+        })?;
+
+        Ok(parser)
+    }
+}
 
 extern "C" {
     fn tree_sitter_elm() -> tree_sitter::Language;
+    fn tree_sitter_ruby() -> tree_sitter::Language;
 }
 
 fn language_elm() -> tree_sitter::Language {
     unsafe { tree_sitter_elm() }
 }
+fn language_ruby() -> tree_sitter::Language {
+    unsafe { tree_sitter_ruby() }
+}
 
-fn parser(
-    language: tree_sitter::Language,
-) -> Result<tree_sitter::Parser, tree_sitter::LanguageError> {
-    let mut parser = tree_sitter::Parser::new();
+#[derive(Debug)]
+enum LanguageError {
+    UnknownLanguage,
+}
 
-    parser.set_language(language)?;
-
-    Ok(parser)
+impl Display for LanguageError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            LanguageError::UnknownLanguage => {
+                write!(f, "Unknown language. Try one of \"elm\" or \"ruby\"")
+            }
+        }
+    }
 }
