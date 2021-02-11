dae91b204ce1bdb125be123cfe6d60b118ad2c41
Brian Hicks <brian@brianthicks.com>
2021-02-11 17:11:18 -0600

    update languages in the subtrees

--- scripts/update-subtrees.sh
+++ scripts/update-subtrees.sh
@@ -2,3 +2,5 @@
 set -euo pipefail
 
 git subtree pull --squash --prefix vendor/tree-sitter-elm https://github.com/Razzeee/tree-sitter-elm master
+git subtree pull --squash --prefix vendor/tree-sitter-ruby https://github.com/tree-sitter/tree-sitter-ruby master
+git subtree pull --squash --prefix vendor/tree-sitter-javascript https://github.com/tree-sitter/tree-sitter-javascript master
