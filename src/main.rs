71b6897c11c9ce9758e87beba9a682d0a65728ae
Brian Hicks <brian@brianthicks.com>
2021-08-10 06:02:10 -0500

    create a filesystem walker from the opts

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use anyhow::{Context, Result};
-// use rayon::iter::{ParallelBridge, ParallelIterator};
-// use std::path::PathBuf;
+use anyhow::{bail, Context, Result};
+use rayon::iter::{ParallelBridge, ParallelIterator};
 
 mod cli;
 mod language;
@@ -18,25 +17,28 @@ fn try_main() -> Result<()> {
     let opts = Opts::from_args()
         .context("couldn't get a valid configuration from the command-line options")?;
 
-    println!("{:?}", opts);
+    build_walker(&opts)
+        .context("couldn't build a filesystem walker")?
+        .par_bridge()
+        .for_each(|p| println!("{:?}", p));
 
     Ok(())
 }
 
-// fn walker(opts: &Opts) -> Result<ignore::Walk> {
-//     let builder = match opts.paths.split_first() {
-//         Some((first, rest)) => {
-//             let mut builder = ignore::WalkBuilder::new(first);
-//             for path in rest {
-//                 builder.add(path);
-//             }
+fn build_walker(opts: &Opts) -> Result<ignore::Walk> {
+    let builder = match opts.paths.split_first() {
+        Some((first, rest)) => {
+            let mut builder = ignore::WalkBuilder::new(first);
+            for path in rest {
+                builder.add(path);
+            }
 
-//             builder
-//         }
-//         None => bail!("I need at least one file or directory to walk!"),
-//     };
+            builder
+        }
+        None => bail!("I need at least one file or directory to walk!"),
+    };
 
-//     // TODO: git ignore, file matching, et cetera
+    // TODO: git ignore, file matching, et cetera
 
-//     Ok(builder.build())
-// }
+    Ok(builder.build())
+}
