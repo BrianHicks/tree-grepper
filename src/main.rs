865447868efde71af7c17e7bb1ba1c76f598db89
Brian Hicks <brian@brianthicks.com>
2021-08-10 14:55:23 -0500

    read sources

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use anyhow::{bail, Context, Result};
 use rayon::iter::{ParallelBridge, ParallelIterator};
+use std::fs;
 
 mod cli;
 mod language;
@@ -20,9 +21,24 @@ fn try_main() -> Result<()> {
     build_walker(&opts)
         .context("couldn't build a filesystem walker")?
         .par_bridge()
+        .filter_map(|entry_result| match entry_result {
+            Ok(entry) => {
+                if entry.file_type().map(|ft| ft.is_dir()).unwrap_or(true) {
+                    None
+                } else {
+                    Some(
+                        fs::read_to_string(entry.path())
+                            .with_context(|| format!("could not read {}", entry.path().display())),
+                    )
+                }
+            }
+            Err(err) => Some(Err(err).context("could not walk a path")),
+        })
+        // remove directories and read files
+        // parse files according to query
         .for_each(|entry_result| match entry_result {
-            Ok(entry) => println!("{:?}", entry.path()),
-            Err(err) => println!("{:?}", err),
+            Ok(entry) => println!("Read source: {:?}", entry),
+            Err(err) => println!("Problem: {:?}", err),
         });
 
     Ok(())
@@ -45,6 +61,8 @@ fn build_walker(opts: &Opts) -> Result<ignore::Walk> {
         .git_ignore(opts.git_ignore)
         .git_exclude(opts.git_ignore)
         .git_global(opts.git_ignore)
+        // TODO: profile to see if construction the filetype matcher twice is
+        // expensive and maybe don't.
         .types(opts.filetype_matcher()?)
         .build())
 }
