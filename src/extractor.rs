dd051a7316e7c3f92ed6976dd7d52f4870fda051
Brian Hicks <brian@brianthicks.com>
2021-08-12 16:48:58 -0500

    parse a tree

--- src/extractor.rs
+++ src/extractor.rs
@@ -2,7 +2,7 @@ use crate::language::Language;
 use anyhow::{Context, Result};
 use std::fs;
 use std::path::Path;
-use tree_sitter::Query;
+use tree_sitter::{Parser, Query};
 
 #[derive(Debug)]
 pub struct Extractor {
@@ -22,6 +22,21 @@ impl Extractor {
     pub fn extract_from_file(&self, path: &Path) -> Result<()> {
         let source = fs::read_to_string(path)
             .with_context(|| format!("could not read {}", path.display()))?;
+
+        // TODO: this is going to allocate a new parser for every single matched
+        // file. Is this something that we want? Parsers are not thread-safe,
+        // so avoiding this might mean rewriting the core of the program away
+        // from being in Rayon's parallel iterator abstraction.
+        //
+        // Also, do we need to avoid calling self.language.language() repeatedly?
+        let mut parser = Parser::new();
+        parser
+            .set_language(self.language.language())
+            .context("could not set language")?;
+
+        let tree = parser.parse(source, None);
+
+        println!("{:?}", tree);
 
         Ok(())
     }
