408870a2a09fdc33c82d64a1efbf1af224f57525
Brian Hicks <brian@brianthicks.com>
2021-08-13 08:17:30 -0500

    only print files with matches

--- src/extractor.rs
+++ src/extractor.rs
@@ -27,7 +27,11 @@ impl Extractor {
         &self.language
     }
 
-    pub fn extract_from_file(&self, path: &Path, parser: &mut Parser) -> Result<ExtractedFile> {
+    pub fn extract_from_file(
+        &self,
+        path: &Path,
+        parser: &mut Parser,
+    ) -> Result<Option<ExtractedFile>> {
         let source =
             fs::read(path).with_context(|| format!("could not read {}", path.display()))?;
 
@@ -43,36 +47,43 @@ impl Extractor {
 
         let mut cursor = QueryCursor::new();
 
-        Ok(ExtractedFile {
-            file: path.to_path_buf(),
-            matches: cursor
-                .matches(&self.query, tree.root_node(), |node| {
-                    node.utf8_text(&source).unwrap_or("")
-                })
-                .flat_map(|query_match| query_match.captures)
-                .map(|capture| {
-                    let node = capture.node;
+        let extracted_matches = cursor
+            .matches(&self.query, tree.root_node(), |node| {
+                // TODO: do this conditionally if there are no matchers?
+                node.utf8_text(&source).unwrap_or("")
+            })
+            .flat_map(|query_match| query_match.captures)
+            .map(|capture| {
+                let node = capture.node;
 
-                    Ok(ExtractedMatch {
-                        text: node
-                            .utf8_text(&source)
-                            .map(|unowned| unowned.to_string())
-                            .context("could not extract text from capture")?,
-                        start: node.start_position(),
-                        end: node.end_position(),
-                        kind: node.kind(),
-                        // note: the cast here could potentially break if run
-                        // on a 16-bit microcontroller. I don't think this is
-                        // a huge problem, though, since even the gnarliest
-                        // queries I've written have something on the order of
-                        // 20 matches. Nowhere close to 2^16!
-                        //
-                        // TODO: is the clone going to be acceptably fast here?
-                        capture: self.captures[capture.index as usize].clone(),
-                    })
+                Ok(ExtractedMatch {
+                    text: node
+                        .utf8_text(&source)
+                        .map(|unowned| unowned.to_string())
+                        .context("could not extract text from capture")?,
+                    start: node.start_position(),
+                    end: node.end_position(),
+                    kind: node.kind(),
+                    // note: the cast here could potentially break if run
+                    // on a 16-bit microcontroller. I don't think this is
+                    // a huge problem, though, since even the gnarliest
+                    // queries I've written have something on the order of
+                    // 20 matches. Nowhere close to 2^16!
+                    //
+                    // TODO: is the clone going to be acceptably fast here?
+                    capture: self.captures[capture.index as usize].clone(),
                 })
-                .collect::<Result<Vec<ExtractedMatch>>>()?,
-        })
+            })
+            .collect::<Result<Vec<ExtractedMatch>>>()?;
+
+        if extracted_matches.is_empty() {
+            Ok(None)
+        } else {
+            Ok(Some(ExtractedFile {
+                file: path.to_path_buf(),
+                matches: extracted_matches,
+            }))
+        }
     }
 }
 
