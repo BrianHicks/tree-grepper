644aefea3bb8cc74ec626eb166e84d94cd2f6096
Brian Hicks <brian@brianthicks.com>
2021-08-17 11:58:11 -0500

    use --squash in all the updates

--- scripts/update-subtrees.sh
+++ scripts/update-subtrees.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-git subtree pull --prefix vendor/tree-sitter-elm https://github.com/elm-tooling/tree-sitter-elm main
-git subtree pull --prefix vendor/tree-sitter-haskell https://github.com/tree-sitter/tree-sitter-haskell master
-git subtree pull --prefix vendor/tree-sitter-javascript https://github.com/tree-sitter/tree-sitter-javascript master
-git subtree pull --prefix vendor/tree-sitter-ruby https://github.com/tree-sitter/tree-sitter-ruby master
-git subtree pull --prefix vendor/tree-sitter-rust https://github.com/tree-sitter/tree-sitter-rust master
-git subtree pull --prefix vendor/tree-sitter-typescript https://github.com/tree-sitter/tree-sitter-typescript master
+git subtree pull --squash --prefix vendor/tree-sitter-elm https://github.com/elm-tooling/tree-sitter-elm main
+git subtree pull --squash --prefix vendor/tree-sitter-haskell https://github.com/tree-sitter/tree-sitter-haskell master
+git subtree pull --squash --prefix vendor/tree-sitter-javascript https://github.com/tree-sitter/tree-sitter-javascript master
+git subtree pull --squash --prefix vendor/tree-sitter-ruby https://github.com/tree-sitter/tree-sitter-ruby master
+git subtree pull --squash --prefix vendor/tree-sitter-rust https://github.com/tree-sitter/tree-sitter-rust master
+git subtree pull --squash --prefix vendor/tree-sitter-typescript https://github.com/tree-sitter/tree-sitter-typescript master
