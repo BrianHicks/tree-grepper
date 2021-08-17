bc6f5d5ef5af53da24e5f5c374db093b2455f19d
Brian Hicks <brian@brianthicks.com>
2021-08-17 08:30:15 -0500

    redo update script to use subtrees

--- scripts/update-submodules.sh
+++ scripts/update-submodules.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -xeuo pipefail
 
-git submodule foreach git pull
+git subtree pull --prefix vendor/tree-sitter-elm https://github.com/elm-tooling/tree-sitter-elm main
+git subtree pull --prefix vendor/tree-sitter-haskell https://github.com/tree-sitter/tree-sitter-haskell master
+git subtree pull --prefix vendor/tree-sitter-javascript https://github.com/tree-sitter/tree-sitter-javascript master
+git subtree pull --prefix vendor/tree-sitter-ruby https://github.com/tree-sitter/tree-sitter-ruby master
+git subtree pull --prefix vendor/tree-sitter-rust https://github.com/tree-sitter/tree-sitter-rust master
+git subtree pull --prefix vendor/tree-sitter-typescript https://github.com/tree-sitter/tree-sitter-typescript master
