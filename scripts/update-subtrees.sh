8cb70b93d2e7e7e7ab03bdb9dbb0dd1eca8e6283
Brian Hicks <brian@brianthicks.com>
2021-12-08 11:45:39 -0600

    make this less repetitious

--- scripts/update-subtrees.sh
+++ scripts/update-subtrees.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-git subtree pull --squash --prefix vendor/tree-sitter-cpp https://github.com/tree-sitter/tree-sitter-cpp master --message "update vendored tree-sitter-cpp"
-git subtree pull --squash --prefix vendor/tree-sitter-elm https://github.com/elm-tooling/tree-sitter-elm main --message "update vendored tree-sitter-elm"
-git subtree pull --squash --prefix vendor/tree-sitter-haskell https://github.com/tree-sitter/tree-sitter-haskell master --message "update vendored tree-sitter-haskell"
-git subtree pull --squash --prefix vendor/tree-sitter-javascript https://github.com/tree-sitter/tree-sitter-javascript master --message "update vendored tree-sitter-javascript"
-git subtree pull --squash --prefix vendor/tree-sitter-ruby https://github.com/tree-sitter/tree-sitter-ruby master --message "update vendored tree-sitter-ruby"
-git subtree pull --squash --prefix vendor/tree-sitter-rust https://github.com/tree-sitter/tree-sitter-rust master --message "update vendored tree-sitter-rust"
-git subtree pull --squash --prefix vendor/tree-sitter-typescript https://github.com/tree-sitter/tree-sitter-typescript master --message "update vendored tree-sitter-typescript"
+update_subtree() {
+  NAME="${1:-}"
+  REPO_URL="${2:-}"
+  BRANCH="${3:-master}"
+
+  if test -z "$NAME" || test -z "$REPO_URL"; then
+    echo "USAGE: update_subtree NAME REPO_URL"
+    return 1
+  fi
+
+  git subtree pull --squash --prefix "vendor/$NAME" "$REPO_URL" "$BRANCH" --message "update vendored $NAME"
+}
+
+update_subtree tree-sitter-cpp https://github.com/tree-sitter/tree-sitter-cpp
+update_subtree tree-sitter-elm https://github.com/elm-tooling/tree-sitter-elm main
+update_subtree tree-sitter-haskell https://github.com/tree-sitter/tree-sitter-haskell
+update_subtree tree-sitter-javascript https://github.com/tree-sitter/tree-sitter-javascript
+update_subtree tree-sitter-ruby https://github.com/tree-sitter/tree-sitter-ruby
+update_subtree tree-sitter-rust https://github.com/tree-sitter/tree-sitter-rust
+update_subtree tree-sitter-typescript https://github.com/tree-sitter/tree-sitter-typescript
